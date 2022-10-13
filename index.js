b59f2de3835a864355d77ee4cdb3076ea17971a5
Arunodaya Singh <arunodaya.singh.611@gmail.com>
2022-10-13 10:25:28 +0530

    mongo backend

--- index.js
+++ index.js
@@ -1,92 +1,107 @@
-import express from "express"
-import mysql from "mysql"
-import cors from "cors"
+const express = require("express"); 
 
-const app = express()
+const app = express(); 
+const PORT = process.env.PORT || 3000; 
 
-const db= mysql.createConnection({
-    host: "localhost",
-    user:"root",
-    password:"123456",
-    database:"test"
-})
+// For testing purposes 
+app.get("/", (req, res) => { 
+    res.send("<h2>It's Working!</h2>"); 
+}); 
 
-
-app.use(express.json());
-app.use(cors())
-
-app.get("/", (req,res)=>{
-    res.json("hello this is backend");
-})
+app.listen(PORT, () => { 
+    console.log(`API is listening on port ${PORT}`); 
+});
 
 
-app.get("/users", (req,res)=>{
-const q= "SELECT * FROM users"
-db.query(q,(err,data)=>{
-    if(err) return res.json(err);
-       return res.json(data);
-})
-})
+// import express from "express"
+// import mysql from "mysql"
+// import cors from "cors"
 
-app.get("/users/:id", (req,res)=>{
-    const userId = req.params.id;
-    const q=  "SELECT *  FROM users WHERE id = ?";
+// const app = express()
 
-
-    db.query(q,[userId], (err,data) =>{
-
-        if(err) return res.json(err);
-        return res.json(data)
-    })
-})
+// const db= mysql.createConnection({
+//     host: "localhost",
+//     user:"root",
+//     password:"123456",
+//     database:"test"
+// })
 
 
+// app.use(express.json());
+// app.use(cors())
 
-app.post("/users", (req,res)=>{
-    const q = "INSERT INTO users (`firstname`,`lastname`,`email`) VALUES (?)";
-    const values= [
-        req.body.firstname,
-        req.body.lastname,
-        req.body.email,
-    ];
-
-    db.query(q,[values], (err,data) => {
-        if(err) return res.json(err);
-        return res.json("users has been crated successfully");
-    })
-})
+// app.get("/", (req,res)=>{
+//     res.json("hello this is backend");
+// })
 
 
-app.delete("/users/:id", (req,res)=>{
-    const userId = req.params.id;
-    const q = "DELETE FROM users WHERE id = ?";
+// app.get("/users", (req,res)=>{
+// const q= "SELECT * FROM users"
+// db.query(q,(err,data)=>{
+//     if(err) return res.json(err);
+//        return res.json(data);
+// })
+// })
 
-    db.query(q,[userId], (err,data) =>{
-
-        if(err) return res.json(err);
-        return res.json("user has been deleted successfully")
-    })
-})
-
-app.put("/users/:id", (req,res)=>{
-    const userId = req.params.id;
-    const q = "UPDATE users SET `firstname` = ?, `lastname` = ?, `email` = ? WHERE id = ?";
-
-    const values= [
-        req.body.firstname,
-        req.body.lastname,
-        req.body.email,
-    ];
+// app.get("/users/:id", (req,res)=>{
+//     const userId = req.params.id;
+//     const q=  "SELECT *  FROM users WHERE id = ?"
 
 
-    db.query(q,[...values,userId], (err,data) =>{
+//     db.query(q,[userId], (err,data) =>{
 
-        if(err) return res.json(err);
-        return res.json("user has been updated successfully")
-    })
-})
+//         if(err) return res.json(err);
+//         return res.json(data)
+//     })
+// })
 
 
-app.listen(8800, ()=>{
-    console.log("connected to backend!");
-})
+
+// app.post("/users", (req,res)=>{
+//     const q = "INSERT INTO users (`firstname`,`lastname`,`email`) VALUES (?)";
+//     const values= [
+//         req.body.firstname,
+//         req.body.lastname,
+//         req.body.email,
+//     ];
+
+//     db.query(q,[values], (err,data) => {
+//         if(err) return res.json(err);
+//         return res.json("users has been crated successfully");
+//     })
+// })
+
+
+// app.delete("/users/:id", (req,res)=>{
+//     const userId = req.params.id;
+//     const q = "DELETE FROM users WHERE id = ?";
+
+//     db.query(q,[userId], (err,data) =>{
+
+//         if(err) return res.json(err);
+//         return res.json("user has been deleted successfully")
+//     })
+// })
+
+// app.put("/users/:id", (req,res)=>{
+//     const userId = req.params.id;
+//     const q = "UPDATE users SET `firstname` = ?, `lastname` = ?, `email` = ? WHERE id = ?";
+
+//     const values= [
+//         req.body.firstname,
+//         req.body.lastname,
+//         req.body.email,
+//     ];
+
+
+//     db.query(q,[...values,userId], (err,data) =>{
+
+//         if(err) return res.json(err);
+//         return res.json("user has been updated successfully")
+//     })
+// })
+
+
+// app.listen(8800, ()=>{
+//     console.log("connected to backend!");
+// })
